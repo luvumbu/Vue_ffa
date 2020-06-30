7a947a2932377fa7ea145c91bdf7ff46b1274e43
Luvumbu <luvumbu.n@gmail.com>
2020-06-30 06:47:27 +0200

    add outils

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+	<title>Document</title>
+</head>
+<body>
+	<header>
+			<?php 
+					include("vue/header.php");
+			 ?>
+	</header>
+	<section>
+			<?php 
+					include("vue/section.php");
+			 ?>
+	</section>
+	<footer>
+		<?php 
+					include("vue/footer.php");
+		?>
+	</footer>
+	  <?php 
+				  include("package/links.php");
+		?>
+		<i class="fas fa-book-dead"></i>
+	 
+		<head><link rel="stylesheet" href="package/css/all.css">
+</body>
+</html>
